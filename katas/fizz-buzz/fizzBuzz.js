938e826f802189a7573fbb2b3acfa6e740f0c8b0
Arthur Vargas <arthurvargas17@gmail.com>
2024-05-17 19:20:23 -0300

    corrigindo testes da função `fizzBuzz`

--- katas/fizz-buzz/fizzBuzz.js
+++ katas/fizz-buzz/fizzBuzz.js
@@ -1,10 +1,11 @@
 /*
- * Escreva um programa que imprime os números de 1 a 100, mas para múltiplos de 3
+ * Escreva um programa que retorna uma lista com os números de 1 a 100, mas para múltiplos de 3
  * imprime "Fizz" ao invés do número e para múltiplos de 5 imprime "Buzz".
  * Para números que são múltiplos de ambos, imprime "FizzBuzz".
+ * Todos os elementos da lista devem ser strings
  */
 
-function fizzBuzz(num) {
+function fizzBuzz() {
   // Função para imprimir FizzBuzz de 1 a 100
 }
 
--- katas/fizz-buzz/fizzBuzz.test.js
+++ katas/fizz-buzz/fizzBuzz.test.js
@@ -1,13 +1,13 @@
 const fizzBuzz = require("./fizzBuzz");
 
 test("FizzBuzz", () => {
-  const results = [];
-  for (let i = 1; i <= 100; i++) {
-    results.push(fizzBuzz(i));
-  }
+  const results = fizzBuzz();
 
   expect(results[2]).toBe("Fizz"); // 3
   expect(results[4]).toBe("Buzz"); // 5
   expect(results[14]).toBe("FizzBuzz"); // 15
   expect(results[29]).toBe("FizzBuzz"); // 30
+  expect(results[0]).toBe("1"); // 1
+  expect(results[98]).toBe("Fizz"); // 99
+  expect(results[99]).toBe("Buzz"); // 100
 });
